7e78c20ded3c5e14211812bb77da2dac58e29ed2
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2018-02-22 21:56:10 -0500

    Blue UI

---
+++ app/src/main/java/btm/app/BleecardUI/DeviceScanActivity.java
@@ -0,0 +1,15 @@
+package btm.app.BleecardUI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import btm.app.R;
+
+public class DeviceScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_scan);
+    }
+}
