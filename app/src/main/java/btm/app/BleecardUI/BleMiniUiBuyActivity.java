265f837bf82f63e26b76e35810a377ae806be330
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2018-02-26 20:44:56 -0500

    Buy Product by Line

--- app/src/main/java/btm/app/BleecardUI/BleMiniUiBuyActivity.java
+++ app/src/main/java/btm/app/BleecardUI/BleMiniUiBuyActivity.java
@@ -1,6 +1,5 @@
 package btm.app.BleecardUI;
 
-
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattService;
 import android.content.BroadcastReceiver;
@@ -9,21 +8,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -34,27 +31,13 @@ import btm.app.DataHolder.DataHolderBleBuy;
 import btm.app.DataHolder.DataHolderBleData;
 import btm.app.R;
 
-public class BleMiniUI extends AppCompatActivity {
+public class BleMiniUiBuyActivity extends AppCompatActivity {
 
     private final static String TAG = DeviceControlActivity.class.getSimpleName();
     Context context = this;
     public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
     public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
     private int[] RGBFrame = {0,0,0};
-
-    private TextView isSerial;
-    private TextView mConnectionState;
-    private TextView mDataField;
-    private TextView text1, text2, text3, text4, text5;
-            TextView ble_id;
-    private TextView pr1, pr2, pr3, pr4, pr5;
-
-    private SeekBar mRed, mGreen, mBlue;
-    private String mDeviceName;
-    private String mDeviceAddress;
-    String action = "list";
-
-    Button b1, b2, b3, b4, b5;
 
     private BluetoothLeService mBluetoothLeService;
     private boolean mConnected = false;
@@ -90,6 +73,21 @@ public class BleMiniUI extends AppCompatActivity {
         }
     };
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ble_mini_ui_buy);
+
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        initBleDataBuyConnect();
+    }
+
     // Handles various events fired by the Service.
     // ACTION_GATT_CONNECTED: connected to a GATT server.
     // ACTION_GATT_DISCONNECTED: disconnected from a GATT server.
@@ -103,13 +101,12 @@ public class BleMiniUI extends AppCompatActivity {
 
             if (BluetoothLeService.ACTION_GATT_CONNECTED.equals(action)) {
                 mConnected = true;
-                updateConnectionState(R.string.connected);
+                //updateConnectionState(R.string.connected);
                 //invalidateOptionsMenu();
             } else if (BluetoothLeService.ACTION_GATT_DISCONNECTED.equals(action)) {
                 mConnected = false;
-                updateConnectionState(R.string.disconnected);
+                //updateConnectionState(R.string.disconnected);
                 //invalidateOptionsMenu();
-                clearUI();
             } else if (BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED.equals(action)) {
                 // Show all the supported services and characteristics on the user interface.
                 displayGattServices(mBluetoothLeService.getSupportedGattServices());
@@ -120,110 +117,11 @@ public class BleMiniUI extends AppCompatActivity {
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ble_mini_ui);
-
-
-        int SDK_INT = android.os.Build.VERSION.SDK_INT;
-        if (SDK_INT > 8) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                    .permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        }
-
-        final Intent intent = getIntent();
-        mDeviceName = intent.getStringExtra(EXTRAS_DEVICE_NAME);
-        mDeviceAddress = intent.getStringExtra(EXTRAS_DEVICE_ADDRESS);
-
-        // Sets up UI references.
-        ((TextView) findViewById(R.id.device_address)).setText(mDeviceAddress);
-        mConnectionState = (TextView) findViewById(R.id.connection_state);
-        // is serial present?
-        isSerial    = (TextView) findViewById(R.id.isSerial);
-        mDataField  = (TextView) findViewById(R.id.data_value);
-        text1       = (TextView) findViewById(R.id.textView1);
-        text2       = (TextView) findViewById(R.id.textView2);
-        text3       = (TextView) findViewById(R.id.textView3);
-        text4       = (TextView) findViewById(R.id.textView4);
-        text5       = (TextView) findViewById(R.id.textView5);
-        ble_id      = (TextView) findViewById(R.id.ble_id);
-
-        pr1         = (TextView) findViewById(R.id.price1);
-        pr2         = (TextView) findViewById(R.id.price2);
-        pr3         = (TextView) findViewById(R.id.price3);
-        pr4         = (TextView) findViewById(R.id.price4);
-        pr5         = (TextView) findViewById(R.id.price5);
-
-        b1 = (Button) findViewById(R.id.button1);
-        b2 = (Button) findViewById(R.id.button2);
-        b3 = (Button) findViewById(R.id.button3);
-        b4 = (Button) findViewById(R.id.button4);
-        b5 = (Button) findViewById(R.id.button5);
-
-        ble_id.setText(DataHolderBleData.getId().toUpperCase());
-
-        Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
-        bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                initBleDataSearch();
-            }
-        }, 3000); //Timer is in ms here.
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                buyProductByLine("1");
-                DataHolderBleBuy.setLiSelected("1");
-            }
-        });
-
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                buyProductByLine("2");
-                DataHolderBleBuy.setLiSelected("2");
-            }
-        });
-
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                buyProductByLine("3");
-                DataHolderBleBuy.setLiSelected("3");
-            }
-        });
-
-        b4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                buyProductByLine("4");
-                DataHolderBleBuy.setLiSelected("4");
-            }
-        });
-
-        b5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                buyProductByLine("5");
-                DataHolderBleBuy.setLiSelected("5");
-            }
-        });
-    }
-
-    private void clearUI() {
-        mDataField.setText(R.string.no_data);
-    }
-
-    @Override
     protected void onResume() {
         super.onResume();
         registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
         if (mBluetoothLeService != null) {
-            final boolean result = mBluetoothLeService.connect(mDeviceAddress);
+            final boolean result = mBluetoothLeService.connect(DataHolderBleData.getMac());
             Log.d(TAG, "Connect request result=" + result);
         }
     }
@@ -258,7 +156,7 @@ public class BleMiniUI extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
             case R.id.menu_connect:
-                mBluetoothLeService.connect(mDeviceAddress);
+                mBluetoothLeService.connect(DataHolderBleData.getMac());
                 return true;
             case R.id.menu_disconnect:
                 mBluetoothLeService.disconnect();
@@ -270,92 +168,25 @@ public class BleMiniUI extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void updateConnectionState(final int resourceId) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                mConnectionState.setText(resourceId);
-            }
-        });
-    }
-
-    private void initBleDataSearch(){
+    private void initBleDataBuyConnect(){
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 Toast.makeText(context, "The ble status is: " + mConnected, Toast.LENGTH_LONG).show();
-                listProduct();
+                buyProductByLine(DataHolderBleBuy.getLiSelected()); //Get LineNumber from the DataHolder and pass it!
             }
         });
     }
 
     private void displayData(String data) {
         if (data != null) {
-            mDataField.setText(data);
+            //mDataField.setText(data);
             if(data.length() > 1) {
-                settingValuesTolines(data);
+                //settingValuesTolines(data);
             }else {
                 Log.d(TAG, "It's returning the 9 number");
             }
         }
-    }
-
-    private void settingValuesTolines(String data) {
-        // This kind of Mini Btm has 5 lines, it goes since 1 to 5
-        // the data is ordered in couples, in e.x.
-        // For a response like: 1201232010 means 12 - 01 - 23 - 20 - 10
-        // it means that for line 1 there are 12 products, line 2 there is 01 (1) product and so on...
-
-        String subStringDataSure = beSureDataString(data);
-        Log.d(TAG, "-> Sub Full String: " + "full string " + subStringDataSure);
-
-        String subString1 = subStringDataSure.substring(0, 2);
-        String subString2 = subStringDataSure.substring(2, 4);
-        String subString3 = subStringDataSure.substring(4, 6);
-        String subString4 = subStringDataSure.substring(6, 8);
-        String subString5 = subStringDataSure.substring(8, 10);
-
-        int value1 = Integer.valueOf(subString1);
-        int value2 = Integer.valueOf(subString2);
-        int value3 = Integer.valueOf(subString3);
-        int value4 = Integer.valueOf(subString4);
-        int value5 = Integer.valueOf(subString5);
-
-        Log.d(TAG, "-> Stock now: " + "Line #1 " + value1);
-        Log.d(TAG, "-> Stock now: " + "Line #2 " + value2);
-        Log.d(TAG, "-> Stock now: " + "Line #3 " + value3);
-        Log.d(TAG, "-> Stock now: " + "Line #4 " + value4);
-        Log.d(TAG, "-> Stock now: " + "Line #5 " + value5);
-
-        text1.setText("Disponible: " + String.valueOf(value1));
-        text2.setText("Disponible: " + String.valueOf(value2));
-        text3.setText("Disponible: " + String.valueOf(value3));
-        text4.setText("Disponible: " + String.valueOf(value4));
-        text5.setText("Disponible: " + String.valueOf(value5));
-
-        try {
-            data = new btm.app.Network.NetActions(context).getBlePrice("5001");
-            Log.d(TAG, " oKHttp response: " + data);
-
-            pr1.setText(data);
-            pr2.setText(data);
-            pr3.setText(data);
-            pr4.setText(data);
-            pr5.setText(data);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private String beSureDataString(String data) {
-         if(data.contains("/")){
-             String sub = data.substring(0, 9);
-             return sub + "0";
-         }else {
-             return data;
-         }
     }
 
     // Demonstrates how to iterate through the supported GATT Services/Characteristics.
@@ -377,10 +208,11 @@ public class BleMiniUI extends AppCompatActivity {
 
             // If the service exists for HM 10 Serial, say so.
             if(SampleGattAttributes.lookup(uuid, unknownServiceString) == "HM 10 Serial") {
-                isSerial.setText("Yes, serial :-)");
+                //isSerial.setText("Yes, serial :-)");
+                Log.d(TAG, "-> " + "Yes, serial :-)");
             }
-            else {  isSerial.setText("No, serial :-(");
-
+            else {
+                Log.d(TAG, "-> " + "No, serial :-(");
             }
             currentServiceData.put(LIST_UUID, uuid);
             gattServiceData.add(currentServiceData);
@@ -398,20 +230,6 @@ public class BleMiniUI extends AppCompatActivity {
         intentFilter.addAction(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED);
         intentFilter.addAction(BluetoothLeService.ACTION_DATA_AVAILABLE);
         return intentFilter;
-    }
-
-    // on change of bars write char
-    private void listProduct() {
-        String str = "0b64a3cd3e6099b8ba9c59183906ee37";
-        Log.d(TAG, "Sending result = " + str);
-        final byte[] tx = hexStringToByteArray(str);
-        Log.d(TAG, "Sending byte[] = " + Arrays.toString(tx));
-
-        if(mConnected) {
-            characteristicTX.setValue(tx);
-            mBluetoothLeService.writeCharacteristic(characteristicTX);
-            mBluetoothLeService.setCharacteristicNotification(characteristicRX,true);
-        }
     }
 
     private void buyProductByLine(final String lineNumber) {
@@ -443,12 +261,12 @@ public class BleMiniUI extends AppCompatActivity {
             mBluetoothLeService.setCharacteristicNotification(characteristicRX,true);
         }
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                listProduct();
-            }
-        }, 3000); //Timer is in ms here.
+        //new Handler().postDelayed(new Runnable() {
+        //    @Override
+        //    public void run() {
+        //        listProduct();
+        //    }
+        //}, 3000); //Timer is in ms here.
     }
 
     public static byte[] hexStringToByteArray(String s) {
@@ -464,4 +282,6 @@ public class BleMiniUI extends AppCompatActivity {
         }
         return data;
     }
+
+
 }
---
+++ app/src/main/java/btm/app/DataHolder/DataHolderBleBuy.java
@@ -0,0 +1,18 @@
+package btm.app.DataHolder;
+
+/**
+ * Created by maguilera on 2/26/18.
+ */
+
+public class DataHolderBleBuy {
+    public static String getLiSelected() {
+        return liSelected;
+    }
+
+    public static void setLiSelected(String liSelected) {
+        DataHolderBleBuy.liSelected = liSelected;
+    }
+
+    private static String liSelected;
+
+}
