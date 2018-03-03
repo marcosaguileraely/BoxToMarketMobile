be331e7b4a945e3d81656cdf2d6b6c024784bf31
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2018-03-03 14:38:36 -0500

    New Webservices added

--- app/src/main/java/btm/app/Network/NetActions.java
+++ app/src/main/java/btm/app/Network/NetActions.java
@@ -230,11 +230,49 @@ public class NetActions {
      * @throws IOException
      * @throws NullPointerException
      */
-    public String btmMiniBuy(String datos) throws IOException, NullPointerException {
+    public String btmMiniPayment(String datos) throws IOException, NullPointerException {
         Log.d(TAG, " Datos card list: " + datos);
-        String url = "https://www.boxtomarket.com/index.php?r=app/btmminiconsumo&token="
+        String url = "https://www.boxtomarket.com/index.php?r=app/btmminiconsumo"
                 + "&token=" + this.tkTime()
-                + "&datos=" + datos;
+                + "&datos=" + this.datoBase64(datos);
+        Log.d("DEV -> NetActions ", url);
+
+        okhttp3.Request requesthttp = new okhttp3.Request.Builder()
+                .url(url)
+                .build();
+
+        okhttp3.Response response = client.newCall(requesthttp).execute();
+        return response.body().string();
+    }
+
+    /**
+     *
+     * @throws IOException
+     * @throws NullPointerException
+     */
+    public String btmMiniActiveSubscription(String datos) throws IOException, NullPointerException {
+        String url = "https://www.boxtomarket.com/index.php?r=app/suscripcionactiva"
+                + "&token=" + this.tkTime()
+                + "&datos=" + this.datoBase64(datos);
+        Log.d("DEV -> NetActions ", url);
+
+        okhttp3.Request requesthttp = new okhttp3.Request.Builder()
+                .url(url)
+                .build();
+
+        okhttp3.Response response = client.newCall(requesthttp).execute();
+        return response.body().string();
+    }
+
+    /**
+     *
+     * @throws IOException
+     * @throws NullPointerException
+     */
+    public String btmMiniChargeBack(String datos) throws IOException, NullPointerException {
+        String url = "https://www.boxtomarket.com/index.php?r=app/btmminidevolucion"
+                + "&token=" + this.tkTime()
+                + "&datos=" + this.datoBase64(datos);
         Log.d("DEV -> NetActions ", url);
 
         okhttp3.Request requesthttp = new okhttp3.Request.Builder()
