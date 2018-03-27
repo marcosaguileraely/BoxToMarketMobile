8003fe396f5d26292d5da412a685eef9b74266cd
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2018-03-27 18:21:04 -0500

    Fixing Register UI

--- app/src/main/java/btm/app/RegisterActivity.java
+++ app/src/main/java/btm/app/RegisterActivity.java
@@ -1,7 +1,10 @@
 package btm.app;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.text.Editable;
@@ -12,6 +15,7 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -19,21 +23,32 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.Response;
+
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+
+import btm.app.Adapters.CountriesAdapter;
+import btm.app.Adapters.SubscriptionAdapter;
+import btm.app.Model.Countries;
+import btm.app.Network.NetActions;
 
 public class RegisterActivity extends DataJp {
     public static final String TAG = "DEV -> Register";
     private FragmentTransaction ft;
-    private EditText username, pass_1, pass_2, email, nombres, apellidos, identificacion, celular;
     private Spinner pais;
-    private String datos;
+    private EditText username, pass_1, pass_2, email, nombres, apellidos, identificacion, celular;
     private CheckBox checkbox_terminos;
-    private Button saveData;
+    Button saveData;
+    ArrayList<Countries> listCountries;
+    private CountriesAdapter adapter;
 
-    private EditText editText;
+
     //private String blockCharacterSet = "~#^|$%&*!ñÑáéíóú. ";
-    private String blockCharacterSet = "~#^|$%&*!";
+    String blockCharacterSet = "~#^|$%&*!";
+    String data, countryName;
+    int countryId;
     private Context context = this;
 
     @Override
@@ -41,6 +56,13 @@ public class RegisterActivity extends DataJp {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
 
         username          = (EditText) findViewById(R.id.editText3);
         pass_1            = (EditText) findViewById(R.id.editText4);
@@ -57,9 +79,12 @@ public class RegisterActivity extends DataJp {
         saveData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                catchData(v);
+                catchData();
             }
         });
+
+        listCountries = new ArrayList<Countries>();
+        renderCountryList();
 
         //When username textEdit changes
         username.addTextChangedListener(new TextWatcher() {
@@ -77,21 +102,9 @@ public class RegisterActivity extends DataJp {
             public void afterTextChanged(Editable s) {
                 String nombre = username.getText().toString();
                 if (nombre.length() == 0) {
-
-                    // Creamos el aviso
-                    /*Toast aviso = Toast.makeText(getApplicationContext(),
-                            "Por favor introduce un nombre de Usuario",
-                            Toast.LENGTH_LONG);
-                    aviso.show();*/
                     username.setError("Ingrese un usuario");
 
                 } else if (!nombre.matches("[a-zA-Z0-9_.? ]*")) {
-                    /*Toast aviso = Toast
-                            .makeText(
-                                    getApplicationContext(),
-                                    "No son permitidos los espacios ni los caracteres especiales",
-                                    Toast.LENGTH_LONG);
-                    aviso.show();*/
                     username.setError("No son permitidos los espacios ni los caracteres especiales");
 
                 } else {
@@ -135,17 +148,12 @@ public class RegisterActivity extends DataJp {
         pais.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String country  = parent.getItemAtPosition(position).toString();
+                String country = parent.getItemAtPosition(position).toString();
+                countryName    = listCountries.get(position).getName();
+                countryId      = listCountries.get(position).getId();
+                setInitNumber(countryName);
 
-                Log.d(TAG, "Lenght: "+ getAreaCode(country));
-                if(getAreaCode(country).length() > 4){
-                    celular.setText("");
-                }else {
-                    Log.d(TAG, "areaCode: "+ getAreaCode(country));
-                    celular.setText(getAreaCode(country));
-                    celular.setSelection(getAreaCode(country).length());
-                    celular.requestFocus();
-                }
+                Log.d(TAG, "Country info: "+ countryId + " - " + countryName);
             }
 
             @Override
@@ -155,8 +163,7 @@ public class RegisterActivity extends DataJp {
         });
     }
 
-    public void catchData(final View view){
-        view.setEnabled(false);
+    public void catchData(){
         if(username.getText().toString().contains(" ")){
             Toast.makeText(this, R.string.alerta_usuario_espacio, Toast.LENGTH_SHORT).show();
             return;
@@ -179,7 +186,6 @@ public class RegisterActivity extends DataJp {
 
         if(!checkbox_terminos.isChecked()){
             Toast.makeText(this, R.string.alerta_terminos, Toast.LENGTH_SHORT).show();
-            view.setEnabled(true);
             return;
         }
 
@@ -220,20 +226,21 @@ public class RegisterActivity extends DataJp {
                    +"&apellido2=" +((apellidos_.length > 1)?apellido_2:"-")     //+apellidos.getText().toString()
                    +"&email="+email.getText().toString()
                    +"&celular="+celular.getText().toString()
-                   +"&pais="+pais.getSelectedItemPosition();
+                   +"&pais="+countryId;
+        try {
+            data = new NetActions(context).register(url);
+            Log.d(TAG, " oKHttp response: " + data);
 
-        new btm.app.Request(this).http_get("registrar", url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                // response
-                Log.d("Response", response);
-                if(response.contains("hemos enviado")){
-                    alert_info(response, getString(R.string.Info), android.R.drawable.ic_dialog_info);
-                } else
-                    Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
-                    view.setEnabled(true);
+            if(data.contains("Te hemos enviado un correo electrónico.")){
+                alert_info_message(data, getString(R.string.Info), android.R.drawable.ic_dialog_info);
             }
-        });
+            else{
+                Toast.makeText(getApplicationContext(), data, Toast.LENGTH_LONG).show();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     //Not in use, but I leave it here for future implementations
@@ -275,16 +282,155 @@ public class RegisterActivity extends DataJp {
         return finalAreaCode;
     }
 
-    /*
-    *
-    * FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    public void renderCountryList(){
+        try {
+            data = new NetActions(context).getCountries();
+            Log.d(TAG, " oKHttp response: " + data);
+
+            String[] data_split = data.split("\\|");
+            for (String cc : data_split) {
+                Log.d(TAG, " --> " + cc);
+
+                String[] country_data_split = cc.split(",");
+                for(int i=0; i<country_data_split.length - 1; i++){
+                    String id   = country_data_split[0];
+                    String name = country_data_split[1];
+                    listCountries.add(new Countries(Integer.valueOf(id), name));
+                }
             }
-        });
-    *
-    * */
+            adapter = new CountriesAdapter(context, R.layout.list_item_countries, listCountries);
+            pais.setAdapter(adapter);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void alert_info_message(String res, String title, int icon){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(res)
+                .setIcon(icon)
+                .setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+                .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    public void onCancel(DialogInterface dialog) {
+                        finish();
+                    }
+                });
+
+        alert.show();
+    }
+
+    public void setInitNumber(String inDatum){
+        if(inDatum.equals("Colombia")){
+            celular.setText("+57");
+
+        }if(inDatum.equals("Chile")){
+            celular.setText("+56");
+
+        }if(inDatum.equals("Mexico")){
+            celular.setText("+52");
+
+        }if(inDatum.equals("Estados Unidos")){
+            celular.setText("+1");
+
+        }if(inDatum.equals("España")){
+            celular.setText("+34");
+
+        }if(inDatum.equals("Alemania")){
+            celular.setText("+49");
+
+        }if(inDatum.equals("Argentina")){
+            celular.setText("+54");
+
+        }if(inDatum.equals("Australia")){
+            celular.setText("+61");
+
+        }if(inDatum.equals("Bolivia")){
+            celular.setText("+591");
+
+        }if(inDatum.equals("Brasil")){
+            celular.setText("+55");
+
+        }if(inDatum.equals("Canada")){
+            celular.setText("+1");
+
+        }if(inDatum.equals("Costa Rica")){
+            celular.setText("+506");
+
+        }if(inDatum.equals("China")){
+            celular.setText("+86");
+
+        }if(inDatum.equals("Francia")){
+            celular.setText("+33");
+
+        }if(inDatum.equals("Guatemala")){
+            celular.setText("+502");
+
+        }if(inDatum.equals("Honduras")){
+            celular.setText("+504");
+
+        }if(inDatum.equals("Hong Kong")){
+            celular.setText("+852");
+
+        }if(inDatum.equals("Holanda")){
+            celular.setText("+31");
+
+        }if(inDatum.equals("India")){
+            celular.setText("+91");
+
+        }if(inDatum.equals("Reino Unido")){
+            celular.setText("+44");
+
+        }if(inDatum.equals("Irlanda")){
+            celular.setText("+353");
+
+        }if(inDatum.equals("Italia")){
+            celular.setText("+39");
+
+        }if(inDatum.equals("Japon")){
+            celular.setText("+81");
+
+        }if(inDatum.equals("Nicaragua")){
+            celular.setText("+505");
+
+        }if(inDatum.equals("Panama")){
+            celular.setText("+507");
+
+        }if(inDatum.equals("Paraguay")){
+            celular.setText("+595");
+
+        }if(inDatum.equals("Peru")){
+            celular.setText("+51");
+
+        }if(inDatum.equals("Portugal")){
+            celular.setText("+351");
+
+        }if(inDatum.equals("Suiza")){
+            celular.setText("+41");
+
+        }if(inDatum.equals("Suecia")){
+            celular.setText("+46");
+
+        }if(inDatum.equals("Uruguay")){
+            celular.setText("+598");
+
+        }if(inDatum.equals("Venezuela")){
+            celular.setText("+58");
+
+        }if(inDatum.equals("Ecuador")){
+            celular.setText("+598");
+
+        }if(inDatum.equals("Republica Dominicana")){
+            celular.setText("+1");
+
+        }
+    }
+
+
 }
